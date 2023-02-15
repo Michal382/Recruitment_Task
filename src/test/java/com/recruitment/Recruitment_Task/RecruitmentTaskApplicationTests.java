bfa25ef1ee9dae55287ab95f46b0f18c17a50c0e
Michal <michal.zyw0@gmail.com>
2023-02-15 22:24:50 +0100

    first commit

---
+++ src/test/java/com/recruitment/Recruitment_Task/RecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.Recruitment_Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
