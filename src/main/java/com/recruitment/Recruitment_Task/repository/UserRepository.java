233cb3f4670464e2942376e5af6e6e024c667d10
Michał <michal.zyw0@gmail.com>
2023-02-17 17:31:25 +0100

    CRUD functionality complete

--- src/main/java/com/recruitment/Recruitment_Task/repository/UserRepository.java
+++ src/main/java/com/recruitment/Recruitment_Task/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.recruitment.Recruitment_Task.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends  CrudRepository<User, Integer>{
+    public Long countById(Integer id);
 }
