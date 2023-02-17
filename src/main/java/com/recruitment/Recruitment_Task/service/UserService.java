70b0995c59e0d18b59458baa6c290df35d84d500
Michał <michal.zyw0@gmail.com>
2023-02-17 14:27:34 +0100

    Manage users table

---
+++ src/main/java/com/recruitment/Recruitment_Task/service/UserService.java
@@ -0,0 +1,19 @@
+package com.recruitment.Recruitment_Task.service;
+
+
+import com.recruitment.Recruitment_Task.repository.UserRepository;
+import com.recruitment.Recruitment_Task.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired private UserRepository repo;
+
+    public List<User> listall(){
+        return (List<User>) repo.findAll();
+
+    }
+}
